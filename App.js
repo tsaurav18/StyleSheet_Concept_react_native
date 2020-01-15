8f5f7e3e7fee85abbd6c5170af1c3291b66c98c0
saurav <tsaurav1711@gmail.com>
2020-01-15 23:26:01 +0900

    firstcommit

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {  View,
+
+} from 'react-native';
+
+
+import styles from "./Style"
+const App = () => {
+  return (
+    <>
+      <View style={styles.parent}>
+        <View style={styles.topBlock}>
+        <View style={styles.leftCol}>
+        <View style={[styles.cellOne,styles.base]}/>
+        <View style={[styles.base,styles.cellTwo]}/>
+          </View>
+          <View style={[styles.cellThree, styles.base]}/>
+          </View>
+          <View style={styles.bottomBlock}>
+          <View style={[styles.cellFour, styles.base]}/>
+          <View style={[styles.cellFive, styles.base]}/>
+         <View style= {styles.bottomRight}>
+         <View style={[styles.cellSix, styles.base]}/>
+          <View style={[styles.cellSeven, styles.base]}/>
+         </View>
+        </View>
+      </View>
+    </>
+  );
+};
+
+
+export default App;
